4626b37a275325b020f3efffee9980ef15d2dc60
erickom8_ <erick.cirico@gmail.com>
2023-05-06 02:34:53 -0300

    Gerenciando erros do servidor

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const route = router.get('/', (req, res, next) =>{
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError); //Quando acontece um 'error', chama a função 'onError'
+
 console.log('Server listening on port', port);
 
 function normalizePort(val) {
@@ -35,3 +37,26 @@ function normalizePort(val) {
     
     return false;
 }  
+
+function onError(error) { // Tratativa de erro
+    if (error.syscall !== 'listen'){ 
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port : 
+        'Port ' + port;
+       
+    switch (error.code){
+        case 'EACCES' : // Verifica se é um erro de permissão.
+            console.error(bind + ' requires elevated privileges'); 
+            process.exit(1);
+            break;
+        case 'EADDRINUSE' : //Verifica se é um erro pois já está em uso.
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }    
+}
