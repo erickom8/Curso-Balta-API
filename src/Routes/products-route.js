f11eb5454133615df6a2ff62c45205f1290c1e2a
erickom8_ <erick.cirico@gmail.com>
2023-05-09 10:10:03 -0300

    Listando pelo ID

--- src/Routes/products-route.js
+++ src/Routes/products-route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 
 router.get('/', controller.get);
+router.get('/admin/:id', controller.getById);;
 router.get('/:slug', controller.getBySlug);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
