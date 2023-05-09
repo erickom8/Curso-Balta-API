da8ff4d580ba1e3a6950ae40663981cae57e82d3
erickom8_ <erick.cirico@gmail.com>
2023-05-09 10:28:27 -0300

    Atualizando um produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -41,16 +41,16 @@ exports.getById = (req,res,next) =>{
 
 exports.getByTag = (req,res,next) =>{
     Product
-    .find({
-        tags : req.params.tag,
-        active:true
-    }, 'title description price slug tags')
-    .then(data => {
-        res.status(200).send({data});
+        .find({
+            tags : req.params.tag,
+            active:true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send({data});
 
-    }).catch(e => {
-        res.status(400).send(e);
-    });
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 }
 
 exports.post = ('/', (req, res, next) =>{ 
@@ -71,11 +71,25 @@ exports.post = ('/', (req, res, next) =>{
 });
 
 exports.put = ('/', (req, res, next) =>{ 
-    const id = req.params.id;
-    res.status(200).send({
-        id : id,
-        item : req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id,{
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price : req.body.price,
+                slug : req.body.slug
+            }
+        })
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto atualizado com sucesso!'
+        });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar produto',
+                data : e
+            });
+        });
 });
 
 exports.delete = ('/', (req, res, next) =>{ 
