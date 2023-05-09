4ec0bb849b71f5dbc83d9bbbb27a5283adc2a652
erickom8_ <erick.cirico@gmail.com>
2023-05-09 08:26:41 -0300

    aula antes da de rotas

--- src/app.js
+++ src/app.js
@@ -6,37 +6,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+const indexRoute = require('../src/Routes/index-route'); //Carrega as rotas
+const productRoute = require('../src/Routes/products-route');
+
 app.use(bodyParser.json()); //Todo o conteúdo convertido em json
-app.use(bodyParser.urlencoded({ extended: false })); //Codificar tbm url
-
-const route = router.get('/', (req, res, next) =>{ //Criando Rota
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-const create = router.post('/', (req, res, next) =>{ //Enviar Informações
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) =>{ 
-    const id = req.params.id;
-    res.status(200).send({
-        id:id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) =>{ //Criando Rota
-    res.status(200).send(req.body);
-});
+app.use(bodyParser.urlencoded({ 
+    extended: false
+ })); //Codificar tbm url
 
 
-app.use('/', route); //Atribuindo rota
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute); //Atribuindo rota
+app.use('/products', productRoute);
 
 module.exports = app;
 
