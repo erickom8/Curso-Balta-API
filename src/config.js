75f4d5144ebb05bbf84d407cfd0332c4ecd1bb83
erickom8_ <erick.cirico@gmail.com>
2023-05-10 10:56:30 -0300

    sendgrid q n deu certo

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store';
 
 module.exports = {
     connectionString: 'mongodb+srv://erickom:escirico22@cluster0.nwrg1mx.mongodb.net/?retryWrites=true&w=majority',
-    sendgridKey : "TBD",
+    sendgridKey : "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", //Criando a API p email
     containerConnectionString : "TBD"
 }
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -4,6 +4,8 @@ const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
 
+const emailService = require('../services/email-service');
+
 exports.post = async(req, res, next) =>{ 
     let contract = new ValidationContract();
     contract.hasMinLen(req.body.name, 3, 'O nome deve conter pelo menos 3 caracteres');
@@ -15,12 +17,19 @@ exports.post = async(req, res, next) =>{
         res.status(400).send(contract.errors()).end();
         return;
     }
+
     try {
         await repository.create({
             name : req.body.name,
             email : req.body.email,
             password : md5(req.body.passwords + global.SALT_KEY)
         });
+
+        emailService.send(
+            req.body.email, 
+            'Bem - Vindo ao Node Store', 
+            global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
         });
