632edd2576bfd2a82ca65a03b93bdb0e837a8145
erickom8_ <erick.cirico@gmail.com>
2023-05-11 09:30:49 -0300

    ultima aula pq n deu nada certo

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,6 +3,7 @@
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
+const authService = require('../services/auth-service');
 
 const emailService = require('../services/email-service');
 
@@ -22,7 +23,7 @@ exports.post = async(req, res, next) =>{
         await repository.create({
             name : req.body.name,
             email : req.body.email,
-            password : md5(req.body.passwords + global.SALT_KEY)
+            password : md5(req.body.password + global.SALT_KEY)
         });
 
         emailService.send(
@@ -32,6 +33,40 @@ exports.post = async(req, res, next) =>{
 
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
+        });
+    } catch(e) {
+            res.status(400).send({
+                message: 'Falha ao cadastrar sua requisição',
+                data : e
+            });
+    };
+};
+
+exports.authenticate = async(req, res, next) =>{ 
+    try {
+        const customer = await repository.authenticate({
+            email : req.body.email,
+            password : md5(req.body.password + global.SALT_KEY)
+        });
+
+        if (!customer) { 
+            res.status(404).send({
+                message : "Usuário ou senha inválidos"
+            });
+            return;
+        }
+
+        const token = await authService.generateToken({
+            email : customer.email,
+            name : customer.name
+        });
+
+        res.status(201).send({
+            token : token,
+            data : {
+                email : customer.email,
+                name : customer.name
+            }
         });
     } catch(e) {
             res.status(400).send({
--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -8,3 +8,10 @@ exports.create = async(data) =>{
     await customer.save();
 }
 
+exports.authenticate = async(data) =>{
+    const res = await Customer.findOne({
+        email : data.email, 
+        password : data.password
+    });
+    return res;
+}
